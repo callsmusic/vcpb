98b9841ef3600562305c163a91bbf0c45b31e705
Roj <rojserbest@gmail.com>
2020-12-31 21:11:40 +0300

    Change .yaml to .yml

--- strings/string.py
+++ strings/string.py
@@ -20,7 +20,7 @@ class String:
 
     def reload_strings(self):
         for filename in os.listdir(r"./strings"):
-            if filename.endswith(".yaml"):
+            if filename.endswith(".yml"):
                 language_name = filename[:-5]
                 self.languages[language_name] = yaml.safe_load(
                     open(r"./strings/" + filename, encoding="utf8"))
