6be796ec31eafd124ef6f0bacb5b13c3e208dd4f
Roj <49933115+rojserbest@users.noreply.github.com>
2021-01-04 20:50:08 +0300

    Update ytdl.py

--- ytdl.py
+++ ytdl.py
@@ -49,7 +49,7 @@ def worker():
                 args[3] = info["title"]
                 args[4] = "https://youtu.be/" + info["id"]
                 args[8] = format_dur(
-                    info["duration"], item["s"], item["m"], item["h"], item["d"])
+                    info["duration"])
 
                 if file_name not in os.listdir("downloads"):
                     item["on_start"][0](
