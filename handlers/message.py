ccc8fd151fe0e3c11f241a98c23ab07e32e88671
Roj <rojserbest@gmail.com>
2021-01-08 11:22:59 +0300

    Fix 'm' is not defined

--- handlers/message.py
+++ handlers/message.py
@@ -21,7 +21,7 @@ def message(client, message):
         message.reply_text(_("message_2"))
         return
 
-    message.reply_text(_("message_3"), quote=True)
+    m = message.reply_text(_("message_3"), quote=True)
 
     download(
         (
