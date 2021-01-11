e327418c30e488b7899b1ec66458cc20b1f7fba7
Roj <rojserbest@gmail.com>
2021-01-11 21:53:10 +0300

    Merge pause_resume.py & skip.py

---
+++ handlers/controls.py
@@ -0,0 +1,56 @@
+
+from pyrogram import Client, filters
+from pyrogram.handlers import MessageHandler
+import player
+from helpers import State
+from config import SUDO_FILTER
+from strings import get_string as _
+
+
+@Client.on_message(
+    filters.command("pause", "/") & SUDO_FILTER
+)
+def pause(client, message):
+    if player.STATE in State.Playing:
+        player.STATE = State.Paused
+        player.pause_resume()
+        message.reply_text(_("pause_1"))
+    elif player.STATE == State.Paused:
+        message.reply_text(_("pause_2"))
+    else:
+        message.reply_text(_("pause_3"))
+
+
+@Client.on_message(
+    (
+        filters.command("resume", "/")
+        | filters.command("play", "/")
+    ) & SUDO_FILTER
+)
+def resume(client, message):
+    if player.STATE == State.Paused:
+        player.STATE = State.Playing
+        player.pause_resume()
+        message.reply_text(_("pause_4"))
+    else:
+        message.reply_text(_("pause_5"))
+
+
+__help__ = {
+    "pause": [_("help_pause"), True],
+    "resume": [_("help_resume"), True],
+    "play": [_("help_play"), True],
+    "skip": [_("help_skip"), True]
+}
+
+
+@Client.on_message(
+    filters.command("skip", "/") & SUDO_FILTER
+)
+def skip(client, message):
+    if player.STATE in (State.Playing, State.Streaming, State.Paused):
+        player.STATE = State.Skipped
+        player.abort()
+        message.reply_text(_("skip_1"))
+    else:
+        message.reply_text(_("skip_2"))
