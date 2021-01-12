b07973fbcaa6f11c95fb2bc0fb41c2c61d0f0d91
Roj <rojserbest@gmail.com>
2021-01-12 13:25:58 +0300

    Add the queue command

--- handlers/queue.py
+++ handlers/queue.py
@@ -19,6 +19,36 @@ def clear_queue(client, message):
         message.reply_text(_("error"))
 
 
+@Client.on_message(
+    filters.command("queue", "/")
+)
+@wrap
+def queue(client, message):
+    qsize = player.q.qsize()
+    queue_ = player.q.queue
+    human_queue = _("queue_1").format(qsize) + "\n"
+    count = 1
+
+    for item in queue_:
+        human_queue += _("queue_2").format(
+            count,
+            '<a href="{}">{}</a>'.format(
+                item["url"],
+                item["title"],
+            ),
+            '<a href="tg://user?id={}">{}</a>'.format(
+                item["sent_by_id"],
+                item["sent_by_name"],
+            ),
+            item["dur"],
+        )
+        count += 1
+
+    m = message.reply_text("....")
+    m.edit_text(human_queue)
+
+
 __help__ = {
-    "clearqueue": [_("help_clearqueue"), True]
+    "clearqueue": [_("help_clearqueue"), True],
+    "clearqueue": [_("help_queue"), False]
 }
