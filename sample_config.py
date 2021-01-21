5d1fe6555861ff9e1c3b7a160b5d5410955679f9
Roj <49933115+rojserbest@users.noreply.github.com>
2021-01-21 15:57:16 +0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -8,7 +8,7 @@ API_HASH = "ab1c23def45fg67890h123i45678j9kl"
 # Get this from @Botfather
 TOKEN = "1234567890:ABCdEFgHij1KlMNop_QrStuVWxyzuA-EmXI"
 
-# Your mongodb uri
+# Your MongoDB URI (using a database named "vcpb"), if you don't provide, you can't use the playlist feature and wont see now playing message
 MONGO_DB_URI = ""
 
 # The IDs of the users which can stream, skip, pause and change volume
@@ -24,9 +24,6 @@ GROUP = -1001402753006
 # Users must join the group before using the bot (note: the bot should be admin in the group if you enable this)
 USERS_MUST_JOIN = False
 
-# Send "now playing" messages to the group
-LOG = True
-
 # Choose the preferred language for your bot. If English leave as it is, or change to the code of any supported language.
 LANG = "en"
 
@@ -34,5 +31,5 @@ LANG = "en"
 DUR_LIMIT = 5
 
 # No need to touch the following.
-LOG_GROUP = GROUP if LOG else None
+LOG_GROUP = GROUP if MONGO_DB_URI != "" else None
 SUDO_FILTER = filters.user(SUDO_USERS)
