6ce3cc36846160d227423c97fb4c2be01fd99c81
Roj Serbest <rojserbest@gmail.com>
2021-02-13 18:09:21 +0300

    'ell

--- ytdl.py
+++ ytdl.py
@@ -24,64 +24,64 @@ def worker():
     while True:
         item = q.get()
 
-        # try:
-        info = ydl.extract_info(item["video"], download=False)
+        try:
+            info = ydl.extract_info(item["video"], download=False)
 
-        if (
-            int(info["duration"] / 60) > DUR_LIMIT
-            and item["sender_id"] not in SUDO_USERS
-        ):
-            if "on_duration_limit" in item:
-                if item["on_duration_limit"]:
-                    item["on_duration_limit"]["args"][0] = item["on_duration_limit"]["args"][0].format(
-                        DUR_LIMIT)
-                    run(item["on_duration_limit"])
-            q.task_done()
-        elif info["is_live"]:
-            if "on_is_live_error" in item:
-                if item["on_is_live_error"]:
-                    run(item["on_is_live_error"])
-            q.task_done()
-        else:
-            file_name = info["id"] + "." + info["ext"]
-            _log = item["play_function"]["kwargs"]["log"]
+            if (
+                int(info["duration"] / 60) > DUR_LIMIT
+                and item["sender_id"] not in SUDO_USERS
+            ):
+                if "on_duration_limit" in item:
+                    if item["on_duration_limit"]:
+                        item["on_duration_limit"]["args"][0] = item["on_duration_limit"]["args"][0].format(
+                            DUR_LIMIT)
+                        run(item["on_duration_limit"])
+                q.task_done()
+            elif info["is_live"]:
+                if "on_is_live_error" in item:
+                    if item["on_is_live_error"]:
+                        run(item["on_is_live_error"])
+                q.task_done()
+            else:
+                file_name = info["id"] + "." + info["ext"]
+                _log = item["play_function"]["kwargs"]["log"]
 
-            if file_name not in os.listdir("downloads"):
-                if "on_start" in item:
-                    if item["on_start"]:
-                        run(item["on_start"])
+                if file_name not in os.listdir("downloads"):
+                    if "on_start" in item:
+                        if item["on_start"]:
+                            run(item["on_start"])
+                    if _log:
+                        open("downloads/" + info["id"] + ".png", "wb+").write(
+                            requests.get(info["thumbnails"][-1]["url"]).content
+                        )
+                    ydl.download([item["video"]])
+
                 if _log:
-                    open("downloads/" + info["id"] + ".png", "wb+").write(
-                        requests.get(info["thumbnails"][-1]["url"]).content
+                    _log["kwargs"]["photo"] = generate_image(
+                        "downloads/" + info["id"] + ".png",
+                        info["title"],
+                        item["sender_name"]
                     )
-                ydl.download([item["video"]])
 
-            if _log:
-                _log["kwargs"]["photo"] = generate_image(
-                    "downloads/" + info["id"] + ".png",
-                    info["title"],
-                    item["sender_name"]
+                run(
+                    item["play_function"],
+                    file="downloads/" + file_name,
+                    title=info["title"],
+                    duration=format_duration(info["duration"]),
+                    url="https://youtu.be/" + info["id"],
+                    log=_log,
                 )
 
-            run(
-                item["play_function"],
-                file="downloads/" + file_name,
-                title=info["title"],
-                duration=format_duration(info["duration"]),
-                url="https://youtu.be/" + info["id"],
-                log=_log,
-            )
+                if "on_end" in item:
+                    if item["on_end"]:
+                        run(item["on_end"])
 
-            if "on_end" in item:
-                if item["on_end"]:
-                    run(item["on_end"])
-
+                q.task_done()
+        except:
+            if "on_error" in item:
+                if item["on_error"]:
+                    run(item["on_error"])
             q.task_done()
-        # except:
-        #     if "on_error" in item:
-        #         if item["on_error"]:
-        #             run(item["on_error"])
-        #     q.task_done()
 
 
 threading.Thread(target=worker, daemon=True).start()
