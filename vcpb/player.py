519e9f885d84e336f2843603589c0f39ea246a8c
Roj <49933115+rojserbest@users.noreply.github.com>
2021-04-05 17:42:51 +0300

    Fix stream stopping

--- vcpb/player.py
+++ vcpb/player.py
@@ -11,18 +11,25 @@ streaming = []
 def worker():
     while True:
         item = queue.get()
-        run(item["on_start"], quote=True)
-        item["log"]["args"][1] = item["log"]["args"][1].format(
-            item["url"],
-            item["title"],
-            item["duration"],
-        )
-        log = run(item["log"])
-        mpv.play(item["file"])
-        mpv.wait_for_playback()
-        run(item["on_end"], quote=True)
-        log.delete()
-        queue.task_done()
+        
+        if "stream" in item:
+            mpv.play(item["url"])
+            mpv.wait_for_playback()
+            play(stream=True, url=item["url"])
+            queue.task_done()
+        else:
+            run(item["on_start"], quote=True)
+            item["log"]["args"][1] = item["log"]["args"][1].format(
+                item["url"],
+                item["title"],
+                item["duration"],
+            )
+            log = run(item["log"])
+            mpv.play(item["file"])
+            mpv.wait_for_playback()
+            run(item["on_end"], quote=True)
+            log.delete()
+            queue.task_done()
 
 
 threading.Thread(target=worker, daemon=True).start()
@@ -38,7 +45,7 @@ def is_streaming():
 
 def stream(url: str):
     streaming.append(0)
-    mpv.play(url)
+    play(stream=True, url=url)
 
 
 def stop_streaming() -> bool:
