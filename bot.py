0e638b69cff531385213dc4f1a570f548c2c4b6a
sangramghangale <81154807+sangramghangale@users.noreply.github.com>
2021-04-22 08:31:49 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,37 @@
+from pyrogram import Client
+from config import API_ID, API_HASH, TOKEN, LOG_GROUP
+
+
+app = Client(
+    "my_account",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=TOKEN,
+    plugins={"root": "handlers", "exclude": None if LOG_GROUP else ["playlist"]}
+)
+
+
+if __name__ == "__main__":
+    import os
+    import sys
+    from threading import Thread
+    from pyrogram import idle, filters
+    from pyrogram.handlers import MessageHandler
+    import player
+    from config import SUDO_FILTER
+    from strings import _
+
+    def stop_and_restart():
+        app.stop()
+        os.system("git pull")
+        os.execl(sys.executable, sys.executable, *sys.argv)
+
+    @app.on_message(filters.command("r", "/") & SUDO_FILTER)
+    def restart(client, message):
+        message.reply_text(_("bot"))
+        Thread(
+            target=stop_and_restart
+        ).start()
+
+    app.start()
+    idle()
